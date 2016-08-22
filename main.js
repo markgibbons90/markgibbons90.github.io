3b7b47d16f0d30be9d09db61fb5dbe0fca8b48bf
Mark <mg8404@my.bristol.ac.uk>
2016-08-23 00:21:33 +0100

    Playing around with sticky nav

--- main.js
+++ main.js
@@ -12,4 +12,21 @@
         document.getElementById('landing-page').style.height = windowHeight;
     }
 
+    var height = window.innerHeight;
+    var nav = document.getElementById('main-nav');
+    // TODO: refactor this
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > height) {
+            nav.style.position = 'fixed';
+            nav.style.top = 0;
+            nav.style.left = 0;
+            nav.style.right = 0;
+        } else {
+            nav.style.position = 'initial';
+            delete nav.style.top;
+            delete nav.style.left;
+            delete nav.style.right;
+        }
+    });
+
 })();
